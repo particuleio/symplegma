824b573b41e2eb4938de8547e7e6a453c41edc1e
ArchiFleKs <lefevre.kevin@gmail.com>
2018-10-26 16:23:17 +0200

    fix: fix init-aws script

--- scripts/init-aws.sh
+++ scripts/init-aws.sh
@@ -3,5 +3,5 @@
 mkdir -p inventory/aws/${1}
 cp -arn inventory/sample/* inventory/aws/${1}/.
 cp -arn contrib/aws/terraform/env/terraform.tfvars inventory/aws/.
-cp -arn contrib/aws/terraform/env/sample/symplegma inventory/aws/${1}/tf_module_symplegma
+cp -arn contrib/aws/terraform/env/sample/symplegma/. inventory/aws/${1}/tf_module_symplegma
 ln -sf ../../../contrib/aws/inventory/aws.py inventory/aws/${1}/.
