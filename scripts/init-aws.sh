bb7c6470c55c4791f5fc21ecbde1bb08c579ad28
ArchiFleKs <lefevre.kevin@gmail.com>
2018-10-29 12:09:06 +0100

    fix: aws init script relative path

--- scripts/init-aws.sh
+++ scripts/init-aws.sh
@@ -5,5 +5,5 @@ cp -arn inventory/sample/* inventory/aws/"${1}"/.
 cp -arn contrib/aws/terraform/env/terraform.tfvars inventory/aws/.
 cp -arn contrib/aws/terraform/env/sample/symplegma/. inventory/aws/"${1}"/tf_module_symplegma
 cp -arn contrib/aws/extra_vars.yml inventory/aws/"${1}"/.
-ln -sf contrib/aws/scripts/symplegma-ansible.sh inventory/aws/"${1}"/.
+ln -sf ../../../contrib/aws/scripts/symplegma-ansible.sh inventory/aws/"${1}"/.
 ln -sf ../../../contrib/aws/inventory/aws.py inventory/aws/"${1}"/.
